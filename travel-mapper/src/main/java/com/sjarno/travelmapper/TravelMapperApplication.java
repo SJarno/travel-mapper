7dfefb8af98175cea8f5da16da5b938c71ff2749
SJarno <jarno.saastamoinen@hotmail.com>
2021-12-21 12:43:17 +0200

    initial commit

---
+++ travel-mapper/src/main/java/com/sjarno/travelmapper/TravelMapperApplication.java
@@ -0,0 +1,13 @@
+package com.sjarno.travelmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelMapperApplication.class, args);
+	}
+
+}
---
+++ travel-mapper/src/test/java/com/sjarno/travelmapper/TravelMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjarno.travelmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
